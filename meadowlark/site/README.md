e02270c25b9d5ed2e9983662ea3feeaac32f3ba7
Prastiyo (pop) <adiprass28@gmail.com>
2022-03-04 10:58:13 +0800

    Code coverage

---
+++ meadowlark/site/README.md
@@ -0,0 +1,2 @@
+- Jest provides some automated code coverage analysis.
+  `npm test -- --coverage`
--- meadowlark/site/lib/handlers.js
+++ meadowlark/site/lib/handlers.js
@@ -2,12 +2,6 @@ const fortune = require('./fortune')
 
 exports.home = (req, res) => res.render('home')
 
-// exports.about = (req, res) => {
-//   res.render('about', {
-//     fortune: fortune.getFortune(),
-//   })
-// }
-
 exports.about = (req, res) =>
   res.render('about', { fortune: fortune.getFortune() })
 
